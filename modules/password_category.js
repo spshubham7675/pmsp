9efb4f552d7a8041bb2ae1ef8f158d72ba1fe99b
spshubham7675 <89580099+spshubham7675@users.noreply.github.com>
2021-08-26 22:54:57 +0530

    Update password_category.js

--- modules/password_category.js
+++ modules/password_category.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://sp:Shubham1998@cluster0.s1ncj.mongodb.net/PMS?retryWrites=true&w=majority', {useNewUrlParser: true,useUnifiedTopology: true, useCreateIndex: true});
+mongoose.connect('mongodb+srv://sp:Shubham1998@pms.lovvn.mongodb.net/pms?retryWrites=true&w=majority', {useNewUrlParser: true,useUnifiedTopology: true, useCreateIndex: true});
 var conn =mongoose.Collection;
 var passcatSchema =new mongoose.Schema({
     password_category: {type:String, 
